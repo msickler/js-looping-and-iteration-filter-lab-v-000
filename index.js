cc591d4e1a049249444f2dd88335fc7836bd58f9
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-19 02:41:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-
+  drivers.filter(function (drivers)) {
+    return driver.toLowerCase() === string.toLowerCase()
+  }
 }
